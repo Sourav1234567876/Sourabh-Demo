87b335db89a8461b92bb359f8caf730041368aac
Sourabh123 <Souravbhola123@gmail.com>
2023-11-24 11:34:42 -0500

    Added feature

---
+++ src/SourabhPracticeJava/Practice.java
@@ -0,0 +1,9 @@
+public class Practice
+{
+// git checkout -b feature1 (create a branch name feature1)
+//add the changes to this branch
+// git add . (to add all the files)
+    //git commit -m (commit all the changes)
+    // git push (to add all the changes from Local to Remote)
+
+}
